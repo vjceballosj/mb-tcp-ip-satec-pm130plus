84c23962e62425710df69ace83d08876e4acf25b
Valentín Ceballos <108242764+vjceballosj@users.noreply.github.com>
2025-09-01 10:36:20 -0500

    spring inizializr

---
+++ pm130plus-back/src/main/java/com/modbus/pm130plus/Pm130plusApplication.java
@@ -0,0 +1,13 @@
+package com.modbus.pm130plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pm130plusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pm130plusApplication.class, args);
+	}
+
+}
---
+++ pm130plus-back/src/test/java/com/modbus/pm130plus/Pm130plusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modbus.pm130plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pm130plusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
